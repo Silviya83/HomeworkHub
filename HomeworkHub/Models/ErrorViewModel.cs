7de67356ad9231041d67c0e74c766794347f15cc
Asus <Asus@DESKTOP-4J5U0MI>
2024-08-14 11:26:09 +0300

    Add project files.

---
+++ HomeworkHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeworkHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
